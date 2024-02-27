2483042213a5fa87b7cc3a590d344ec592927787
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-02-27 15:16:48 +0400

    migrated project to Vite

--- src/components/GenreSelect.js
+++
@@ -1,9 +0,0 @@
-export function GenreSelect({genres, selectedGenre, onSelect}) {
-    return (
-        <div>
-            <h1>Genre Select</h1>
-            {genres.map(genre => <button key={genre} onClick={() => onSelect(genre)}
-                                         style={{backgroundColor: genre === selectedGenre ? 'lightblue' : 'white',}}>{genre}</button>)}
-        </div>
-    );
-}
