0937ebb93902162006994024e265119bfe017e78
Eduard Bagdagulyan <eduard.bagdagulyan2003@gmail.com>
2024-04-01 14:30:20 +0400

    implemented ErrorPage component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './ErrorPage/ErrorPage.tsx'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
+        errorElement: <ErrorPage />,
     },
 ])
 
